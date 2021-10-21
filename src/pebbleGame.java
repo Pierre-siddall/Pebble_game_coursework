550ba6935a1f57fb964968bbd1ac828855fad024
Priya Shah <priya.shah2309@gmail.com>
2021-10-21 13:18:13 +0100

    Added Player Scanner

--- src/pebbleGame.java
+++ src/pebbleGame.java
@@ -16,11 +16,20 @@ public class pebbleGame {
         }
     }
     public static void main(String[] args){
+        int playerNo = 0;
         System.out.println("Welcome to the PebbleGame!!");
         System.out.println("You will be asked to enter the number of players");
         System.out.print("and then for the location of three files in turn containing comma seperated \n integer values for the pebble weights \n");
         System.out.println("THe integer values must be strictly positive.");
         System.out.println("The game will then be simulate, and output written to files in this director");
+        Scanner playerInput = new Scanner(System.in);
+
+        // Check if user has inputted negative numbers
+        while (playerNo <= 0){
+            System.out.println("Please enter the number of players ");
+            playerNo = playerInput.nextInt();
+        }
+        // Create new players
 
 
     }
