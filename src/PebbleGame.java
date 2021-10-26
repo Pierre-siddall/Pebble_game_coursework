95751781262c4af1d07033e6169fa30c9fc61bda
Pierre-siddall <pierre.siddall@gmail.com>
2021-10-26 17:11:41 +0100

    File reading issue solved
    Signed-off-by: Pierre-siddall <pierre.siddall@gmail.com>

--- src/PebbleGame.java
+++ src/PebbleGame.java
@@ -2,18 +2,18 @@ import java.io.File;
 import java.util.Scanner;
 public class PebbleGame {
 
-    private Bag[] blackBags= {new Bag("BB1"),new Bag("BB2"),new Bag("BB3")};
-    private Bag[] whiteBags={new Bag("WB1"),new Bag("WB2"),new Bag("WB3")};
+    private final Bag[] blackBags = {new Bag("BB1"), new Bag("BB2"), new Bag("BB3")};
+    private final Bag[] whiteBags = {new Bag("WB1"), new Bag("WB2"), new Bag("WB3")};
 
     public class Player {
-        public void draw(Bag chosenBag){
-            try{
+        public void draw(Bag chosenBag) {
+            try {
                 return;
-            }catch (RuntimeException e){
+            } catch (RuntimeException e) {
             }
         }
 
-        public void deposit(){
+        public void deposit() {
             try{
                 return;
             }catch (RuntimeException e){
@@ -29,10 +29,17 @@ public class PebbleGame {
         return whiteBags;
     }
 
-    public static void main(String[] args){
+    public static void showWelcomeMessage() {
+        System.out.println("Welcome to the PebbleGame!!");
+        System.out.println("You will be asked to enter the number of players");
+        System.out.print("and then for the location of three files in turn containing comma seperated \n integer values for the pebble weights \n");
+        System.out.println("The integer values must be strictly positive.");
+        System.out.println("The game will then be simulate, and output written to files in this director");
+    }
 
-        Bag[] blackBags= {new Bag("BB1"),new Bag("BB2"),new Bag("BB3")};
-        Bag[] whiteBags={new Bag("WB1"),new Bag("WB2"),new Bag("WB3")};
+    public static void main(String[] args) {
+        Bag[] blackBags = {new Bag("BB1"), new Bag("BB2"), new Bag("BB3")};
+        Bag[] whiteBags = {new Bag("WB1"), new Bag("WB2"), new Bag("WB3")};
         int playerNo = 0;
 
         System.out.println("Welcome to the PebbleGame!!");
@@ -43,7 +50,7 @@ public class PebbleGame {
         Scanner playerInput = new Scanner(System.in);
 
         // Check if user has inputted negative numbers
-        while (playerNo <= 0){
+        while (playerNo <= 0) {
             System.out.println("Please enter the number of players ");
             playerNo = playerInput.nextInt();
         }
